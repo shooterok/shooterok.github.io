8cfc9cca60b0dd809cee55812a52c85987fb738b
shooterok <shooterok@gmail.com>
2014-10-27 17:18:53 +0200

    Update videos.js

--- demo/demoApp/videos.js
+++ demo/demoApp/videos.js
@@ -3,27 +3,27 @@
 
     window.App.videos = [
         {
-            title: 'Big Buck Bunny',
+            title: '1Big Buck Bunny',
             url: 'http://smartimmo.ru/uploaded/big_buck_bunny_480p_h264.mp4',
             type: 'vod'
         },
         {
-            title: 'Elephants Dream',
+            title: '2Elephants Dream',
             url: 'https://archive.org/download/ElephantsDream/ed_1024_512kb.mp4',
             type: 'vod'
         },
         {
-            title: 'Europa plus',
+            title: '3Europa plus',
             url: 'http://europaplus.cdnvideo.ru/europaplus-live/eptv_main.sdp/playlist.m3u8',
             type: 'hls'
         },
         {
-            title: 'PIK TV',
+            title: '4PIK TV',
             url: 'http://phone.pik-tv.com/live/mp4:piktv3pik3tv/playlist.m3u8',
             type: 'hls'
         },
         {
-            title: 'Redbull',
+            title: '5Redbull',
             url: 'http://live.iphone.redbull.de.edgesuite.net/webtvHD.m3u8',
             type: 'hls'
         }
