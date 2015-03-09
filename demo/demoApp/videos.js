18ebfe912e71e491b32720afd03e264af25cdd57
shooterok <shooterok@gmail.com>
2015-03-09 18:37:27 +0200

    Update videos.js

--- demo/demoApp/videos.js
+++ demo/demoApp/videos.js
@@ -3,8 +3,8 @@
 
     window.App.videos = [
         {
-            title: 'джон вик',
-            url: 'http://175h.net/2014/john_wick/1080.mp4',
+            title: 'Судья',
+            url: 'http://fs.to/get/dl/5ykw9cc9cmmczdc77bhnsxdbq.0.521872670.2185543202.1425918877/%D0%A1%D1%83%D0%B4%D1%8C%D1%8F_The.Judge.2014.BDRip.1080p.mkv',
             type: 'vod'
         },
         {
