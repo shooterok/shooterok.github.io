3ccd1dc916af8e30345842fb1d60d49497dc92fb
shooterok <shooterok@gmail.com>
2015-06-01 13:48:26 +0300

    Update videos.js

--- demo/demoApp/videos.js
+++ demo/demoApp/videos.js
@@ -8,22 +8,7 @@
             type: 'vod'
         },
         {
-            title: 'Black.Sails.S02E02',
-            url: 'http://fs.to/get/dl/5ykw9cc9clxr220r6fhvgjqme.0.521872670.0.1423076519/Black.Sails.S02E02.1080p.rus.LostFilm.TV.mkv',
-            type: 'vod'
-        },
-        {
-            title: '3Europa plus',
-            url: 'http://europaplus.cdnvideo.ru/europaplus-live/eptv_main.sdp/playlist.m3u8',
-            type: 'hls'
-        },
-        {
-            title: '4PIK TV',
-            url: 'http://phone.pik-tv.com/live/mp4:piktv3pik3tv/playlist.m3u8',
-            type: 'hls'
-        },
-        {
-            title: '5Redbull',
+            title: 'Redbull',
             url: 'http://live.iphone.redbull.de.edgesuite.net/webtvHD.m3u8',
             type: 'hls'
         }
