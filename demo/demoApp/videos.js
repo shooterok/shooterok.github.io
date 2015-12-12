fed99995df5918140a193b70b2ff8a723538f78d
shooterok <shooterok@gmail.com>
2015-12-12 20:38:01 +0200

    Update videos.js

--- demo/demoApp/videos.js
+++ demo/demoApp/videos.js
@@ -7,6 +7,12 @@
             url: 'http://fs.to/get/dl/6jw62suuow4t9r6l5jnsmdvnq.0.521872670.2185543202.1449943174/Crimson.Peak.2015.D.WEB-DL.1080p.%5B%D0%9F%D0%B8%D1%84%D0%B0%D0%B3%D0%BE%D1%80%5D.mkv',
             type: 'vod'
         },
+        
+        {
+            title: 'Дивергент',
+            url: 'http://www.ex.ua/get/122424825',
+            type: 'vod'
+        },
         {
             title: 'Redbull',
             url: 'http://live.iphone.redbull.de.edgesuite.net/webtvHD.m3u8',
